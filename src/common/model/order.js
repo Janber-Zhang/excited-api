84272d80ed5303b14af3af19f993ba1d2c55aa3a
janber <janber.zhang.0@gmail.com>
2018-05-06 16:49:10 +0800

    test

---
+++ src/common/model/order.js
@@ -0,0 +1,7 @@
+'use strict';
+/**
+ * created by janber 2018-05-05
+ */
+export default class extends think.model.mongo {
+  
+}
---
+++ src/order/controller/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import Base from './base.js';
+
+export default class extends Base {
+  indexAction(){
+    let data = 'order-model';
+    this.success(data);
+  }
+  /**
+   * saveOrder
+   * 保存菜单
+   */
+  async saveorderAction(){
+    let param    = this.post();
+    let orderModel = this.model('order');
+    if (!param.date || !param.data) {
+      this.fail(1001, 'some params is needed');  
+    }
+    let params = {
+      date      : param.date,
+      data      : param.data
+    };
+    let insertId = await orderModel.add(params)
+    this.success({result: true, data: insertId})
+  }
+  /**
+   * getAppRecord Action 获取应用数据记录
+   * @return {Promise} []
+   */
+  async getallAction(){
+  	let orderModel = this.model('order');
+    let param    = this.get();
+    let query_str = {};
+    if (param.date) {
+      query_str.date = param.date;
+    }
+  	let data = await orderModel.where(query_str).select();
+  	this.success({result:true,data: data});
+  }
+}
